752a9789098071baa5a82fa376124e18a4dde9cb
skinnyfromdenine <adamdok53@gmail.com>
2020-07-23 22:58:46 +0700

    added plumber to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const {
     dest,
-    src
+    src,
 } = require('gulp');
 
 let gulp = require('gulp'),
@@ -9,8 +9,8 @@ let gulp = require('gulp'),
     uglify = require('gulp-uglify-es').default,
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    fileinclude = require("gulp-file-include"),
-    imagemin = require("gulp-imagemin"),
+    fileinclude = require('gulp-file-include'),
+    imagemin = require('gulp-imagemin'),
     del = require('del'),
     clean_css = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer'),
@@ -19,7 +19,8 @@ let gulp = require('gulp'),
     webp = require('gulp-webp'),
     webphtml = require('gulp-webp-html'),
     webpcss = require('gulp-webpcss'),
-    gulpStylelint = require('gulp-stylelint');
+    plumber = require('gulp-plumber');
+gulpStylelint = require('gulp-stylelint');
 
 
 let project_folder = require('path').basename(__dirname);
@@ -30,23 +31,23 @@ let path = {
         js: project_folder + '/js/',
         style: project_folder + '/css/',
         img: project_folder + '/img/',
-        fonts: project_folder + '/fonts/'
+        fonts: project_folder + '/fonts/',
     },
     app: {
         html: ['app/*.html', '!' + 'app/_*.html'],
         js: 'app/js/script.js',
         style: 'app/scss/style.scss',
         img: 'app/images/**/*.{jpg,png,svg,gid,ico,webp}',
-        fonts: 'app/fonts/**/*.ttf'
+        fonts: 'app/fonts/**/*.ttf',
     },
     watch: {
         html: 'app/**/*.html',
         js: 'app/js/**/*.js',
         style: 'app/scss/**/*.scss',
         img: 'app/images/**/*.{jpg,png,svg,gid,ico,webp}',
-        fonts: 'app/fonts/**/*.*'
+        fonts: 'app/fonts/**/*.*',
     },
-    clean: project_folder
+    clean: project_folder,
 };
 
 
@@ -54,77 +55,83 @@ function html() {
     return src(path.app.html)
         .pipe(fileinclude())
         .pipe(webphtml())
-        .pipe(dest(path.build.html))
+        .pipe(dest(path.build.html));
 }
 
 function css() {
     return gulp.src(path.app.style)
+        .pipe(plumber())
         .pipe(
             scss({
-                outputStyle: 'compressed'
-            })
+                outputStyle: 'compressed',
+            }),
         )
         .pipe(
             autoprefixer({
-                overrideBrowserslist: ['last 8 versions']
-            })
+                overrideBrowserslist: ['last 8 versions'],
+            }),
         )
         .pipe(
             rename({
-                suffix: '.min'
-            })
+                suffix: '.min',
+            }),
         )
         .pipe(webpcss())
         .pipe(clean_css())
-        .pipe(dest(path.build.style))
+        .pipe(dest(path.build.style));
 }
 
 function libsCss() {
     return src([
-            'node_modules/normalize.css/normalize.css',
-            'node_modules/slick-carousel/slick/slick.css',
-            'node_modules/slick-carousel/slick/slick-theme.css',
-        ])
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/slick-carousel/slick/slick-theme.css',
+    ])
+        .pipe(plumber())
         .pipe(concat('libs.min.css'))
         .pipe(clean_css())
-        .pipe(dest(path.build.style))
+        .pipe(dest(path.build.style));
 }
 
 function linting() {
     return src(path.app.style)
+        .pipe(plumber())
         .pipe(gulpStylelint({
             reporters: [{
                 formatter: 'string',
-                console: true
-            }]
+                console: true,
+            }],
         }));
 }
 
 function js() {
     return src(path.app.js)
+        .pipe(plumber())
         .pipe(eslint({}))
         .pipe(eslint.format())
         .pipe(babel())
         .pipe(uglify())
-        .pipe(dest(path.build.js))
+        .pipe(dest(path.build.js));
 }
 
 function libsJs() {
     return src([
-            'node_modules/slick-carousel/slick/slick.js'
-        ])
+        'node_modules/slick-carousel/slick/slick.js',
+    ])
+        .pipe(plumber())
         .pipe(concat('libs.min.js'))
         .pipe(fileinclude())
         .pipe(uglify())
-        .pipe(dest(path.build.js))
+        .pipe(dest(path.build.js));
 }
 
 function images() {
     return src(path.app.img)
+        .pipe(plumber())
         .pipe(
             webp({
-                quality: 70
-            })
+                quality: 70,
+            }),
         )
         .pipe(dest(path.build.img))
         .pipe(src(path.app.img))
@@ -132,17 +139,18 @@ function images() {
             imagemin({
                 progressive: true,
                 svgoPlugins: [{
-                    removeViewBox: false
+                    removeViewBox: false,
                 }],
-                optimizationLevel: 3
-            })
+                optimizationLevel: 3,
+            }),
         )
-        .pipe(dest(path.build.img))
+        .pipe(dest(path.build.img));
 }
 
 function fonts() {
     return src(path.app.fonts)
-        .pipe(dest(path.build.fonts))
+        .pipe(plumber())
+        .pipe(dest(path.build.fonts));
 }
 
 function serve(cb) {
@@ -150,22 +158,22 @@ function serve(cb) {
         server: project_folder,
         notify: false,
         open: true,
-        cors: true
-    })
+        cors: true,
+    });
 
-    gulp.watch([path.watch.html], gulp.series(html)).on('change', server.reload)
-    gulp.watch([path.watch.style], gulp.series(css)).on('change', server.reload)
-    gulp.watch([path.watch.js], gulp.series(js)).on('change', server.reload)
-    gulp.watch([path.watch.img], gulp.series(images)).on('change', server.reload)
+    gulp.watch([path.watch.html], gulp.series(html)).on('change', server.reload);
+    gulp.watch([path.watch.style], gulp.series(css)).on('change', server.reload);
+    gulp.watch([path.watch.js], gulp.series(js)).on('change', server.reload);
+    gulp.watch([path.watch.img], gulp.series(images)).on('change', server.reload);
 
-    return cb()
+    return cb();
 }
 
 function clean(params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(html, css, libsCss, js, libsJs, images, fonts, linting))
+let build = gulp.series(clean, gulp.parallel(html, css, libsCss, js, libsJs, images, fonts, linting));
 let watch = gulp.parallel(serve, build);
 
 exports.images = images;
